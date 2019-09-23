169b80672f846f39d0ec4d7deae266fd7a6fe642
Thiago de Campos <thiago.campos@dev.getmore.com.br>
2019-09-23 12:00:48 -0300

    Add theming

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
-import React from 'react';
-import MainsScreen from './components/screens/MainScreen/MainScreen';
+import React from 'react'
+import MainsScreen from './components/screens/MainScreen/MainScreen'
+import theme from './Theme'
+import { ThemeProvider } from '@material-ui/styles'
 
-function App() {
+function App () {
   return (
-    <MainsScreen />
-  );
+    <ThemeProvider theme={theme}>
+      <MainsScreen />
+    </ThemeProvider>
+
+  )
 }
 
-export default App;
+export default App
---
+++ client/src/Theme.js
@@ -0,0 +1,13 @@
+import { createMuiTheme } from '@material-ui/core/styles'
+
+const theme = createMuiTheme({
+  colors: {
+    lightGrey1: '#D9D9D9',
+    lightGrey2: '#737373',
+    darkGrey1: '#404040',
+    darkGrey2: '#262626',
+    darkGrey3: '#0D0D0D'
+  }
+})
+
+export default theme
