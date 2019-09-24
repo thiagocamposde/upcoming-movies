994081ef73378272e4bbe13429d76aa69739b49c
Thiago de Campos <thiago.campos@dev.getmore.com.br>
2019-09-24 10:21:51 -0300

    Adding Router

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,14 @@ import React from 'react'
 import MainsScreen from './components/screens/MainScreen'
 import theme from './Theme'
 import { ThemeProvider } from '@material-ui/styles'
+import { BrowserRouter } from 'react-router-dom'
 
 function App () {
   return (
     <ThemeProvider theme={theme}>
-      <MainsScreen />
+      <BrowserRouter>
+        <MainsScreen />
+      </BrowserRouter>
     </ThemeProvider>
 
   )
--- client/src/components/screens/MainScreen/MainScreen.js
+++ client/src/components/screens/MainScreen/MainScreen.js
@@ -1,22 +1,41 @@
 import React, { useEffect, useState } from 'react'
 import { getTmdbApiConfiguration } from '../../../api/configuration.api'
 import UpcomingMovies from '../UpcomingMovies'
+import MovieDetails from '../MovieDetails'
+import { Switch, Route } from 'react-router-dom'
 
 const MainsScreen = ({ classes }) => {
   const [tmdbConfig, setTmdbConfig] = useState(null)
+  const [tmdbConfigIsloading, setTmdbConfigIsLoading] = useState(true)
 
   useEffect(() => {
     const fetchTmdbConfiguration = async () => {
       const response = await getTmdbApiConfiguration()
       setTmdbConfig(response.data)
+      setTmdbConfigIsLoading(false)
     }
 
     fetchTmdbConfiguration()
   }, [])
 
+  if (tmdbConfigIsloading) return null
+
   return (
     <div className={classes.root}>
-      {tmdbConfig && <UpcomingMovies baseUrl={tmdbConfig.images.base_url} />}
+      <Switch>
+        <Route
+          exact path='/' render={props => (
+            <UpcomingMovies {...props} baseUrl={tmdbConfig.images.base_url} />
+          )}
+        />
+        <Route
+          exact path='/movie/details' render={props => (
+            <MovieDetails {...props} baseUrl={tmdbConfig.images.base_url} />
+          )}
+        />
+        )}
+      />
+      </Switch>
     </div>
   )
 }
