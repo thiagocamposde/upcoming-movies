919abf40e08249e58d825c255b2605c86414f37d
Thiago de Campos <thiago.campos@dev.getmore.com.br>
2019-09-26 11:09:05 -0300

    Heroku settup

--- app.js
+++ app.js
@@ -16,5 +16,16 @@ const configurationRoutes = require('./routes/configuration')
 app.use(movieRoutes)
 app.use(configurationRoutes)
 
+if (process.env.NODE_ENV === 'production') {
+  // Exprees will serve up production assets
+  app.use(express.static('client/build'))
+
+  // Express serve up index.html file if it doesn't recognize route
+  const path = require('path')
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
+
 const PORT = process.env.PORT || 5000
 app.listen(PORT)
